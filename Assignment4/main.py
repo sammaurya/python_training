98f92a1dd14ecbcfe2d1fce69f237a44326cec78
sammaurya <sammaurya196@gmail.com>
2020-02-16 23:19:58 +0530

    assignment done

--- Assignment4/main.py
+++ Assignment4/main.py
@@ -6,19 +6,23 @@ from my_date import date_op
 
 birth_date = input("Enter birth date (yyyy/mm/dd) : ")
 
-birth_date = "".join(char if char.isdigit() else " " for char in birth_date).split()
+birth_date = "".join(char if char.isdigit()
+                     else " " for char in birth_date).split()
 
 year = int(birth_date[0])
 month = int(birth_date[1])
 day = int(birth_date[2])
 
+if not date_op.check_date(year, month, day):
+    print("Invalid date!!!")
+    exit()
+
 print("Today is ", date_op.get_current_datetime())
 
-age = date_op.get_age(year,month,day)
+age = date_op.get_age(year, month, day)
 
 print("Age is ", age)
 
 print("Date of birth is ", date_op.birth_date(age))
 
-print("Time until 50 is ", date_op.time_until_50(year, month, day))
-
+print("Time until 50 is ", date_op.time_to_50(year, month, day))
--- Assignment4/my_date/date_op.py
+++ Assignment4/my_date/date_op.py
@@ -4,75 +4,104 @@
 #   -- date_operations.py
 #   -- __init__.py
 #      1. get_current_datetime
-#      2. When entering age in string: 19 years, 5 months, 20 days. It should 
+#      2. When entering age in string: 19 years, 5 months, 20 days. It should
 #      return birth_date
 #      3. When entering birth_date. Return age in the above format.
-#      4. When entering birth_date: Return how many time will person take to 
+#      4. When entering birth_date: Return how many time will person take to
 #      turn 50 years. Return string in above format.
 
 from datetime import datetime, timedelta
 from dateutil import relativedelta
+
+
+def is_leap_year(year):
+    if((year % 4 == 0) and (year % 100 != 0) or year % 400 == 0):
+        return True
+    return False
+
+
+def check_date(year, month, day):
+    '''Check validity of any given date
+    '''
+    if is_leap_year(year):
+        if month == 2:
+            if day >= 1 and day <= 29:
+                return True
+    else:
+        if month == 2:
+            if day >= 1 and day <= 28:
+                return True
+
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        if day >= 1 and day <= 31:
+            return True
+    elif month in (4, 6, 9, 11):
+        if day >= 1 and day <= 30:
+            return True
+
+    return False
+
 
 def get_current_datetime():
     return datetime.today()
 
 
 def birth_date(age):
+    today = get_current_datetime().date()
     # extracting years, months and days from the age string
     age = "".join(char if char.isdigit() else " " for char in age).split()
-    years = int(age[0])
+    years = today.year - int(age[0])
     months = int(age[1])
     days = int(age[2])
 
-    # # calculating total days
-    # total_days = (years*12 + months)*30 + days
+    if months > today.month:
+        months = (today.month + 12) - months
+        years -= 1
+    elif months < today.month:
+        months = today.month - months
 
-    # # get current date and time
-    # today = get_current_datetime()
+    if months == 3 and today.day < days:
+        days -= 2
 
-    # # calculating previous date based on total_days
-    # birth_date = today - timedelta(days=total_days)
-
-    today = get_current_datetime()
-    weeks = (years + months/12) * 52
-    birth_date = today - timedelta(weeks= weeks, days=days)
-    print(birth_date)
+    birth_date = datetime(years, months, today.day)
+    birth_date = birth_date - timedelta(days=days)
 
     # formating birth_date in string
-    birth_date = "{0}-{1}-{2}".format(birth_date.year, birth_date.month, birth_date.day)
-    
+    birth_date = "{0}-{1}-{2}".format(birth_date.year,
+                                      birth_date.month, birth_date.day)
+
     return birth_date
 
 
-birth_date("22 years, 5 months, 2 days")
-
 def get_age(year, month, day):
     # get current date and time
-    today = get_current_datetime()
+    today = get_current_datetime().date()
 
     # birth year, month, day to datetime class
     birth_date = datetime(year, month, day)
 
     # calculating relative difference between current date and birth date
     age = relativedelta.relativedelta(today, birth_date)
-   
+
     # making string with years, months, and days
-    age = "{0} years, {1} months, {2} days".format(age.years, age.months, age.days)
-    
+    age = "{0} years, {1} months, {2} days".format(
+        age.years, age.months, age.days)
+
     # returning string
     return age
 
-def time_until_50(year, month, day):
-    today = datetime.today() 
-    birth_date = datetime(year,month,day)
-    date_to_50 = birth_date + timedelta(days=(50*365))
-    remaining_datetime = relativedelta.relativedelta(date_to_50, today)
 
+def time_to_50(year, month, day):
+    today = get_current_datetime().date()
+    if month == 2 and day == 29:
+        day = 28
+
+    date_to_50 = datetime(year+50, month, day)
+    time_to_50 = relativedelta.relativedelta(date_to_50, today)
+    years = time_to_50.years
+    months = time_to_50.months
+    days = time_to_50.days
     # making string with years, months, and days
-    remaining_datetime = "{0} years, {1} months, {2} days".format(remaining_datetime.years,
-             remaining_datetime.months, remaining_datetime.days)
-    
-   
-    return remaining_datetime
-
-
+    time_to_50 = "{0} years, {1} months, {2} days".format(
+        years, months, days)
+    return time_to_50
